2f6157a47595c94629f09878d10ebafc3d7d45ee
Warzone01 <kirill.trumpeter@gmail.com>
2018-12-11 12:52:17 +0300

    Add download files

--- song/models.py
+++ song/models.py
@@ -3,27 +3,9 @@ from slugger import AutoSlugField
 
 
 class Category(models.Model):
-    # ALL = "Основные"
-    # CHILD = 'Детские'
-    # CHRISTMAS = 'Рождественские'
-    # EASTER = 'Пасхальные'
-    #
-    # song_genre = (
-    #     (ALL, "Основные"),
-    #     (CHILD, "Детские"),
-    #     (CHRISTMAS, "Рождественские"),
-    #     (EASTER, 'Пасхальные')
-    # )
     name = models.CharField('Name', max_length=20, null=True)
     slug = AutoSlugField(populate_from='name', null=True)
     image = models.ImageField(null=True, blank=True)
-
-    # choice = models.CharField(
-    #     max_length=20,
-    #     choices=song_genre,
-    #     default=ALL,
-    #     blank=True
-    # )
 
     def __str__(self):
         return self.name
--- song/urls.py
+++ song/urls.py
@@ -1,5 +1,6 @@
+from django.conf.urls.static import static
 from django.urls import path
-
+from django.conf import settings
 from . import views
 
 urlpatterns = [
