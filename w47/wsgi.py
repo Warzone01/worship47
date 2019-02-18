7efa1f7fc327a6b69b9188e5f74ffdb3a5b8e58d
Aleksey <trash4@yandex.ru>
2019-02-18 19:33:30 +0300

    some refactor

--- w47/wsgi.py
+++ w47/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Worship project.
+WSGI config for w47 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Worship.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "w47.settings")
 
 application = get_wsgi_application()
