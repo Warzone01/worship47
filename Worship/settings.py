52f1ac9c07e17c5873585729665f96573a877fcb
Warzone01 <kirill.trumpeter@gmail.com>
2019-03-11 09:15:12 +0300

    account

--- Worship/settings.py
+++ Worship/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'worship',
     'song',
+    'account',
     'ckeditor',
     'django_registration',
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+from django.template import loader
+from django.db.models import Q
+
+
+def account(request):
+    return render(request, 'account/account.html')
+
