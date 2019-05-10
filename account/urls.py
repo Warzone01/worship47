75be883e41f0d0292c7307a7ffce226af3b1090a
Aleksey <trash4@yandex.ru>
2019-05-10 11:48:55 +0300

    add migrations

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.account, name='account'),
+    # path('accounts/login/$', LoginView.as_view(authentication_form=AuthenticationForm), name='login'),
+    # path('accounts/', include('django.contrib.auth.urls')),
 
 ]
