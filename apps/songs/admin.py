b49225e8739d6eefdddd16dd9b816e0d617312cb
Aleksey <trash4@yandex.ru>
2019-11-26 17:57:43 +0300

    add filtering list of songs by category's slug

--- apps/songs/admin.py
+++ apps/songs/admin.py
@@ -7,9 +7,16 @@ class LinkInLine(admin.StackedInline):
     model = Link
     extra = 1
 
+
 @register(Song)
 class SongAdmin(admin.ModelAdmin):
+    list_display = ['title', 'title_eng', 'categ']
     inlines = [LinkInLine]
 
+    def categ(self, obj):
+        categs = []
+        for c in obj.category.all():
+            categs.append(f"{c.name} ({c.slug})")
+        return ' || '.join(categs)
 
 admin.site.register(Link)
--- apps/songs/views.py
+++ apps/songs/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, \
     PermissionRequiredMixin
 from django.contrib.auth.views import LoginView
+from django.shortcuts import get_object_or_404, get_list_or_404
 from django.urls import reverse
 from django.views.generic import ListView, DetailView, UpdateView, CreateView
 
@@ -10,8 +11,19 @@ from songs.models import Song
 
 class SongList(ListView):
     model = Song
+    allow_empty = False
     paginate_by = 10
     context_object_name = 'songs'
+    queryset = Song.objects.all()
+
+    def get_queryset(self):
+        # Filter by categorie's slug
+        qs = super(SongList, self).get_queryset()
+        categ = self.request.GET.get("categ")
+        if categ:
+            return qs.filter(category__slug__in=[categ])
+        else:
+            return qs
 
 
 class SongDetail(LoginRequiredMixin, DetailView):
