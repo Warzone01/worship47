5c5e92f16c17f5f4c15b7246ae2a81770af4e5b1
Aleksey <trash4@yandex.ru>
2020-06-24 10:34:08 +0300

    cleanup and fix update form

--- apps/songs/forms.py
+++ apps/songs/forms.py
@@ -1,8 +1,4 @@
 from django import forms
-from extra_views import InlineFormSetFactory, UpdateWithInlinesView, CreateWithInlinesView, ModelFormSetView, \
-    FormSetView, InlineFormSetView
-from extra_views.generic import GenericInlineFormSetView
-
 from .models import Song
 
 
@@ -11,27 +7,4 @@ class SongForm(forms.ModelForm):
     class Meta:
         model = Song
         exclude = ['created', 'modified']
-
-
-#
-# class CreateFields(CreateWithInlinesView):
-#     model = Song
-#     inlines = [ChordsView, LinkView]
-#     # template_name = 'songs/song_create_form.html'
-#
-#
-# class UpdateFields(UpdateWithInlinesView):
-#     model = Song
-#     inlines = [ChordsView, LinkView]
-#     # template_name = 'songs/song_update_form.html'
-
-
-# class FormSet(FormSetView):
-#     form_class = LinkForm
-#     template_name = 'song_create_form.html'
-
-
-
-
-
 
